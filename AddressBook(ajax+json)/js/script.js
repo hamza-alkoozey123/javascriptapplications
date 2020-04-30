26e9eccfa716c62ae2f25936d41d16687dee86c0
Hamza Alkoozey <hamze.matador@gmail.com>
2020-04-30 21:30:53 +0300

    new

---
+++ AddressBook(ajax+json)/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    $ajaxUtils.sendGetRequest("../json/data.json", function(res) {
+        const firstName = res.person[0].name;
+        console.log(firstName);
+    },
+    true
+    );
+})
