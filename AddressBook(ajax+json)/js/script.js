3a804836aa7309fc9344f4838e41e95d389c936f
Hamza Alkoozey <hamze.matador@gmail.com>
2020-08-02 14:29:55 +0300

    new

--- AddressBook(ajax+json)/js/script.js
+++ AddressBook(ajax+json)/js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 
-    $ajaxUtils.sendGetRequest("../json/data.json", function(res) {
+    $ajaxUtils.sendGetRequest("json/data.json", function(res) {
         const firstName = res.person[0].name;
         console.log(firstName);
     },
