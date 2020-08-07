2dd189b423bb814a8eeec0813355a0725f1faa79
Hamza Alkoozey <hamze.matador@gmail.com>
2020-08-07 13:43:54 +0300

    new

--- Slideshow/js/script.js
+++ Slideshow/js/script.js
@@ -1,114 +1,58 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  const itemInput = document.querySelector("#itemInput");
-  const feedback = document.querySelector("#feedback");
-  const List = document.querySelector("#itemsList");
-  var todoItems = [];
+document.addEventListener("DOMContentLoaded", function () {
+  const prev = document.querySelector("#previous");
+  const next = document.querySelector("#next");
+  const slides = document.getElementsByClassName("mySlides");
+  const links = document.getElementsByClassName("links");
 
-  function handleItem(itemVal) {
-    const items = document.querySelectorAll(".itemElement");
+  console.log("hi");
 
-    items.forEach(function (element) {
-      if (element.querySelector("#itemName").textContent === itemVal) {
-        element.querySelector("#check").addEventListener("click", function () {
-          element.querySelector("#itemName").classList.toggle("completed");
-        });
+  index = 0;
+  slides[0].classList.add("d-block");
+  links[0].classList.add("active");
 
-        element.querySelector("#delete").addEventListener("click", function () {
-          List.removeChild(element);
-          removeItem(itemVal);
-        });
+  function showSlide(currentIndex) {
+    for (i = 0; i < slides.length; i++) {
+      slides[i].classList.remove("d-block");
+      slides[i].classList.add("d-none");
+      links[i].classList.remove("active");
+    }
+    slides[currentIndex].classList.remove("d-none");
+    slides[currentIndex].classList.add("d-block");
 
-        element.querySelector("#edit").addEventListener("click", function () {
-          element.querySelector("#itemName").classList.toggle("d-none");
-          element.querySelector("#itemTools").classList.toggle("ml-auto");
-          element.querySelector("#editInput").classList.toggle("d-none");
-          element.querySelector(
-            "#editInput input"
-          ).value = element.querySelector("#itemName").textContent;
-          element.querySelector("#edit").classList.toggle("d-none");
-          element.querySelector("#submit").classList.toggle("d-none");
-        });
-
-        element.querySelector("#itemName").addEventListener("click", function () {
-          element.querySelector("#itemName").classList.toggle("d-none");
-          element.querySelector("#itemTools").classList.toggle("ml-auto");
-          element.querySelector("#editInput").classList.toggle("d-none");
-          element.querySelector(
-            "#editInput input"
-          ).value = element.querySelector("#itemName").textContent;
-          element.querySelector("#edit").classList.toggle("d-none");
-          element.querySelector("#submit").classList.toggle("d-none");
-        });
-
-        element.querySelector("#submit").addEventListener("click", function () {
-          element.querySelector(
-            "#itemName"
-          ).textContent = element.querySelector("#editInput input").value;
-          element.querySelector("#submit").classList.toggle("d-none");
-          element.querySelector("#editInput").classList.toggle("d-none");
-          element.querySelector("#edit").classList.toggle("d-none");
-          element.querySelector("#itemName").classList.toggle("d-none");
-        });
-
-        element
-          .querySelector("#editInput input")
-          .addEventListener("keyup", function (event) {
-            if (event.keyCode === 13) {
-              element.querySelector("#submit").click();
-            }
-          });
-
-        console.log(List);
-      }
-    });
+    links[currentIndex].classList.add("active");
   }
 
-  const removeItem = function (itemVal) {
-    const removeIndex = todoItems.indexOf(itemVal);
-    todoItems.splice(removeIndex, 1);
-  };
-
-  function getList(theItems) {
-    List.innerHTML = " ";
-
-    theItems.forEach((element) => {
-      List.insertAdjacentHTML(
-        "beforeend",
-        "<div class='itemElement mb-3 d-flex justify-content-between'><p id='itemName' class='text-capitalize'>" +
-          element +
-          "</p><div id='editInput' class='d-none'><input class='form-control form-control-sm' type='text'/></div><div id='itemTools'><button id='submit' class='btn btn-sm btn-outline-primary d-none'>Submit</button><button id='edit' class='btn btn-sm btn-outline-primary'>Edit</button><button id='check' class='btn btn-sm btn-outline-success'>Check</button><button id='delete' class='btn btn-sm btn-outline-danger'>Delete</button></div></div>"
-      );
-
-      handleItem(element);
-    });
-  }
-
-  document.querySelector("#addItem").addEventListener("click", function () {
-    var itemVal = itemInput.value;
-    if (itemVal.length === 0) {
-      feedback.innerHTML = "<h1>Input is empty ! please input any value</h1>";
-      feedback.classList.add(
-        "showItem",
-        "text-center",
-        "bg-danger",
-        "rounded",
-        "text-white"
-      );
-      setTimeout(function () {
-        feedback.classList.remove("showItem");
-      }, 3000);
+  prev.addEventListener("click", function () {
+    if (index == 0) {
+      index = slides.length - 1;
     } else {
-      todoItems.push(itemVal);
-      console.log(itemVal);
-      getList(todoItems);
+      index--;
     }
-    itemInput.value = "";
+    showSlide(index);
   });
 
-  //Hitting enter on input box
-  itemInput.addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-      document.querySelector("#addItem").click();
+  next.addEventListener("click", function () {
+    if (index == slides.length - 1) {
+      index = 0;
+    } else {
+      index++;
     }
+    showSlide(index);
   });
+
+  console.log(prev);
+
+  links[0].addEventListener("click", function () {
+    index = 1;
+    showSlide(0);
+  });
+  links[1].addEventListener("click", function () {
+    index = 1;
+    showSlide(1);
+  });
+  links[2].addEventListener("click", function () {
+    index = 2;
+    showSlide(2);
+  });
+
 });
--- Todo List/js/script.js
+++ Todo List/js/script.js
@@ -1,114 +1,115 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  const itemInput = document.querySelector("#itemInput");
-  const feedback = document.querySelector("#feedback");
-  const List = document.querySelector("#itemsList");
-  var todoItems = [];
-
-  function handleItem(itemVal) {
-    const items = document.querySelectorAll(".itemElement");
-
-    items.forEach(function (element) {
-      if (element.querySelector("#itemName").textContent === itemVal) {
-        element.querySelector("#check").addEventListener("click", function () {
-          element.querySelector("#itemName").classList.toggle("completed");
-        });
-
-        element.querySelector("#delete").addEventListener("click", function () {
-          List.removeChild(element);
-          removeItem(itemVal);
-        });
-
-        element.querySelector("#edit").addEventListener("click", function () {
-          element.querySelector("#itemName").classList.toggle("d-none");
-          element.querySelector("#itemTools").classList.toggle("ml-auto");
-          element.querySelector("#editInput").classList.toggle("d-none");
-          element.querySelector(
-            "#editInput input"
-          ).value = element.querySelector("#itemName").textContent;
-          element.querySelector("#edit").classList.toggle("d-none");
-          element.querySelector("#submit").classList.toggle("d-none");
-        });
-
-        element.querySelector("#itemName").addEventListener("click", function () {
-          element.querySelector("#itemName").classList.toggle("d-none");
-          element.querySelector("#itemTools").classList.toggle("ml-auto");
-          element.querySelector("#editInput").classList.toggle("d-none");
-          element.querySelector(
-            "#editInput input"
-          ).value = element.querySelector("#itemName").textContent;
-          element.querySelector("#edit").classList.toggle("d-none");
-          element.querySelector("#submit").classList.toggle("d-none");
-        });
-
-        element.querySelector("#submit").addEventListener("click", function () {
-          element.querySelector(
-            "#itemName"
-          ).textContent = element.querySelector("#editInput input").value;
-          element.querySelector("#submit").classList.toggle("d-none");
-          element.querySelector("#editInput").classList.toggle("d-none");
-          element.querySelector("#edit").classList.toggle("d-none");
-          element.querySelector("#itemName").classList.toggle("d-none");
-        });
-
-        element
-          .querySelector("#editInput input")
-          .addEventListener("keyup", function (event) {
-            if (event.keyCode === 13) {
-              element.querySelector("#submit").click();
-            }
+    const itemInput = document.querySelector("#itemInput");
+    const feedback = document.querySelector("#feedback");
+    const List = document.querySelector("#itemsList");
+    var todoItems = [];
+  
+    function handleItem(itemVal) {
+      const items = document.querySelectorAll(".itemElement");
+  
+      items.forEach(function (element) {
+        if (element.querySelector("#itemName").textContent === itemVal) {
+          element.querySelector("#check").addEventListener("click", function () {
+            element.querySelector("#itemName").classList.toggle("completed");
           });
-
-        console.log(List);
+  
+          element.querySelector("#delete").addEventListener("click", function () {
+            List.removeChild(element);
+            removeItem(itemVal);
+          });
+  
+          element.querySelector("#edit").addEventListener("click", function () {
+            element.querySelector("#itemName").classList.toggle("d-none");
+            element.querySelector("#itemTools").classList.toggle("ml-auto");
+            element.querySelector("#editInput").classList.toggle("d-none");
+            element.querySelector(
+              "#editInput input"
+            ).value = element.querySelector("#itemName").textContent;
+            element.querySelector("#edit").classList.toggle("d-none");
+            element.querySelector("#submit").classList.toggle("d-none");
+          });
+  
+          element.querySelector("#itemName").addEventListener("click", function () {
+            element.querySelector("#itemName").classList.toggle("d-none");
+            element.querySelector("#itemTools").classList.toggle("ml-auto");
+            element.querySelector("#editInput").classList.toggle("d-none");
+            element.querySelector(
+              "#editInput input"
+            ).value = element.querySelector("#itemName").textContent;
+            element.querySelector("#edit").classList.toggle("d-none");
+            element.querySelector("#submit").classList.toggle("d-none");
+          });
+  
+          element.querySelector("#submit").addEventListener("click", function () {
+            element.querySelector(
+              "#itemName"
+            ).textContent = element.querySelector("#editInput input").value;
+            element.querySelector("#submit").classList.toggle("d-none");
+            element.querySelector("#editInput").classList.toggle("d-none");
+            element.querySelector("#edit").classList.toggle("d-none");
+            element.querySelector("#itemName").classList.toggle("d-none");
+          });
+  
+          element
+            .querySelector("#editInput input")
+            .addEventListener("keyup", function (event) {
+              if (event.keyCode === 13) {
+                element.querySelector("#submit").click();
+              }
+            });
+  
+          console.log(List);
+        }
+      });
+    }
+  
+    const removeItem = function (itemVal) {
+      const removeIndex = todoItems.indexOf(itemVal);
+      todoItems.splice(removeIndex, 1);
+    };
+  
+    function getList(theItems) {
+      List.innerHTML = " ";
+  
+      theItems.forEach((element) => {
+        List.insertAdjacentHTML(
+          "beforeend",
+          "<div class='itemElement mb-3 d-flex justify-content-between'><p id='itemName' class='text-capitalize'>" +
+            element +
+            "</p><div id='editInput' class='d-none'><input class='form-control form-control-sm' type='text'/></div><div id='itemTools'><button id='submit' class='btn btn-sm btn-outline-primary d-none'>Submit</button><button id='edit' class='btn btn-sm btn-outline-primary'>Edit</button><button id='check' class='btn btn-sm btn-outline-success'>Check</button><button id='delete' class='btn btn-sm btn-outline-danger'>Delete</button></div></div>"
+        );
+  
+        handleItem(element);
+      });
+    }
+  
+    document.querySelector("#addItem").addEventListener("click", function () {
+      var itemVal = itemInput.value;
+      if (itemVal.length === 0) {
+        feedback.innerHTML = "<h1>Input is empty ! please input any value</h1>";
+        feedback.classList.add(
+          "showItem",
+          "text-center",
+          "bg-danger",
+          "rounded",
+          "text-white"
+        );
+        setTimeout(function () {
+          feedback.classList.remove("showItem");
+        }, 3000);
+      } else {
+        todoItems.push(itemVal);
+        console.log(itemVal);
+        getList(todoItems);
+      }
+      itemInput.value = "";
+    });
+  
+    //Hitting enter on input box
+    itemInput.addEventListener("keyup", function (event) {
+      if (event.keyCode === 13) {
+        document.querySelector("#addItem").click();
       }
     });
-  }
-
-  const removeItem = function (itemVal) {
-    const removeIndex = todoItems.indexOf(itemVal);
-    todoItems.splice(removeIndex, 1);
-  };
-
-  function getList(theItems) {
-    List.innerHTML = " ";
-
-    theItems.forEach((element) => {
-      List.insertAdjacentHTML(
-        "beforeend",
-        "<div class='itemElement mb-3 d-flex justify-content-between'><p id='itemName' class='text-capitalize'>" +
-          element +
-          "</p><div id='editInput' class='d-none'><input class='form-control form-control-sm' type='text'/></div><div id='itemTools'><button id='submit' class='btn btn-sm btn-outline-primary d-none'>Submit</button><button id='edit' class='btn btn-sm btn-outline-primary'>Edit</button><button id='check' class='btn btn-sm btn-outline-success'>Check</button><button id='delete' class='btn btn-sm btn-outline-danger'>Delete</button></div></div>"
-      );
-
-      handleItem(element);
-    });
-  }
-
-  document.querySelector("#addItem").addEventListener("click", function () {
-    var itemVal = itemInput.value;
-    if (itemVal.length === 0) {
-      feedback.innerHTML = "<h1>Input is empty ! please input any value</h1>";
-      feedback.classList.add(
-        "showItem",
-        "text-center",
-        "bg-danger",
-        "rounded",
-        "text-white"
-      );
-      setTimeout(function () {
-        feedback.classList.remove("showItem");
-      }, 3000);
-    } else {
-      todoItems.push(itemVal);
-      console.log(itemVal);
-      getList(todoItems);
-    }
-    itemInput.value = "";
   });
-
-  //Hitting enter on input box
-  itemInput.addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-      document.querySelector("#addItem").click();
-    }
-  });
-});
+  
